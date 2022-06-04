fdad596ad59f6014349a4eecf89426c3cc0e507c
Mwenje <mwenje24@gmail.com>
2022-06-04 20:48:10 +0300

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // Your code here
 const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
 
 function moveDodgerLeft(){
-    var left = dodger.style.left
+    let left = dodger.style.left
     left = parseInt(left)
-    dodger.style.left = `${left-10}px`
+    if(left > 0){
+        dodger.style.left = `${left-10}px`
+    }
 }
 moveDodgerLeft()
 
 function moveDodgerRight(){
-    var left = dodger.style.left
+    let left = dodger.style.left
     left = parseInt(left)
     dodger.style.left = `${left + 10}px`
 }
 moveDodgerRight()
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
