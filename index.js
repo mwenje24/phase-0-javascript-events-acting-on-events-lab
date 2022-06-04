c1624132dcb9fdf58001c652163a94c9833d33b5
Mwenje <mwenje24@gmail.com>
2022-06-04 20:40:03 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    var left = dodger.style.left
+    left = parseInt(left)
+    dodger.style.left = `${left-10}px`
+}
+moveDodgerLeft()
+
+function moveDodgerRight(){
+    var left = dodger.style.left
+    left = parseInt(left)
+    dodger.style.left = `${left + 10}px`
+}
+moveDodgerRight()
